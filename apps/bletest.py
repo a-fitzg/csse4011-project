6ae7a67265efb1b4c23a2a0750a98b8340eb158d
a-fitzg <xanderfitzg2000@gmail.com>
2021-05-23 22:28:18 +1000

    BLE parsing test

--- apps/bletest.py
+++ apps/bletest.py
@@ -3,13 +3,20 @@ from bleak import BleakScanner
 from bleak.backends.device import BLEDevice
 from bleak.backends.scanner import AdvertisementData
 import logging
+import ctypes
 
 logging.basicConfig()
 
 
 def simple_callback(device: BLEDevice, advertisement_data: AdvertisementData):
-    if device.address == "CF:95:D7:62:5F:4D":
-        print(device.address, "RSSI:", device.rssi, advertisement_data)
+    if device.address == "E1:38:D4:CD:DE:AF":
+        rssi_list = []
+        us_list = []
+        adv_bytearray = list(advertisement_data.service_data.values())[0]
+        node_values = [ctypes.c_int8(adv_bytearray[i]).value for i in range(len(adv_bytearray))]
+        rssi_list.extend(node_values[3:15])
+        us_list.extend(node_values[15:17])
+        print("RSSI: ", rssi_list, ",  US: ", us_list)
 
 
 async def run():
@@ -18,9 +25,10 @@ async def run():
 
     while True:
         await scanner.start()
-        await asyncio.sleep(5.0)
+        await asyncio.sleep(0.03)
         await scanner.stop()
 
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(run())
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(run())
